9b9a903f13dcc3be6ea26c39d8c6674d44b983ce
Abudi Mutamba <abudimutamba@users-MacBook-Air.local>
2021-07-13 12:43:08 +0300

    More testing examples

--- calculate.js
+++ calculate.js
@@ -8,8 +8,19 @@ let sum = (num1, num2)  => {
    return num1 + num2
 }
 
+let substract = (num1, num2) =>{
+   if(typeof num1 != 'number') return false
+   if(typeof num2 != 'number') return false
+   return num1 - num2
+}
 //onsole.log(sum(5, 9))
 //console.log(sum(o,9)) //This can be very hard to troubleshoot becuse the sytem compiler won't return
 //console.log(sum(10, '90'))
 
-module.exports = sum;
+
+//module.eports = sum
+//Exports more than one function
+module.exports = {
+   sum,
+   substract
+}
--- calculate.test.js
+++ calculate.test.js
@@ -1,4 +1,5 @@
-// const sum = require('./calculate')
+//const { TestWatcher } = require('jest')
+const {sum, substract} = require('./calculate')
 
 // test ('add 1  + 2 to equal 3', () => {
 //     expect(sum(1, 2)).toBe(3);
@@ -16,7 +17,42 @@
 //     expect(sum(2)).toBe(false);
 // });
 
-test ('returns false if one argument is not a number', () => {
-    expect(sum('3',3)).toBe(false);
+// test ('returns false if one argument is a string ', () => {
+//     expect(sum('3',3)).toBe(false);
+// });
+
+//Subtraction test
+test( 'Subtract 4 - 2 toBe 2', () => {
+   //Testing for the type and vaue 
+   expect (substract(4, 2)).toBe(2)
 });
 
+test( 'Subtract 4 - 2 toBe 2', () => {
+    //Testing for the type and vaue 
+    expect (typeof substract(4, 2)).toBe('number')
+ });
+ 
+ test( 'Subtract "4" - 2 toBe falsy', () => {
+    //Testing for the type and vaue 
+    expect (substract("4", 2)).toBeFalsy()
+ });
+ 
+ test( `Subtract '4' - '2' toBe falsy`, () => {
+    //Testing for the type and vaue 
+    expect (substract('4', '2')).toBeFalsy()
+ });
+ 
+ test( `Subtract 4 - "2" toBe falsy`, () => {
+    //Testing for the type and vaue 
+    expect (substract(4, "2")).toBeFalsy()
+ });
+
+ test( 'Subtract 2 - 2 toBe falsy', () => {
+    //Testing for the type and vaue 
+    expect (substract(2, 2)).toBeFalsy()
+ });
+
+ test( 'Subtract 10 - 10 toBe 0', () => {
+    //Testing for the type and vaue 
+    expect (substract(10, 10)).toBe(0)
+ });
